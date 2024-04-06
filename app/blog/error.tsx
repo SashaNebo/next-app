6e3cd4dccf98ec4f36c3af94692b12289f18681f
Ilya Pribytkov <118817047+SashaNebo@users.noreply.github.com>
2024-04-06 14:03:08 +0300

    basic next app

---
+++ app/blog/error.tsx
@@ -0,0 +1,6 @@
+'use client';
+
+export default function ErrorWrapper({error}: {error: Error}) {
+  return <h1>OOps!!! {error.message}</h1>
+}
+
---
+++ components/TheFooter.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+const TheFooter: FC = () => {
+  return (
+    <footer className='container'>
+      <h5>Created by &copy;IlyaPribytkov</h5>
+    </footer>
+  )
+}
+
+export { TheFooter }
